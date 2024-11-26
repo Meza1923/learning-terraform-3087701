8eb6213539f9823a571fa09643c7029a9bc2ffa0
Meza1923 <97029572+Meza1923@users.noreply.github.com>
2024-11-26 19:39:10 +0200

    guy

--- main.tf
+++ main.tf
@@ -18,11 +18,28 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vcp.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform again"
@@ -33,10 +50,10 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name    = "blog_new"
+  
 
-  vpc_id = data.aws_vpc.default.id
-
+  vpc_id = module.blog_vpc.vpc_id
+  name    = "blog"
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
