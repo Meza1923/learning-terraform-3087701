ba4e53df3f914e00ed765e3dbd2762e1db6bb09c
Meza1923 <97029572+Meza1923@users.noreply.github.com>
2024-11-27 19:35:40 +0200

    autoscaling

--- main.tf
+++ main.tf
@@ -31,19 +31,25 @@ module "blog_vpc" {
 }
 
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+  
+  name     = blog
+  min_size = 1
+  max_size = 1
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_groups_args  = module.blog_alb
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id               = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform again"
-  }
 }
 
-module "alb" {
+
+
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
