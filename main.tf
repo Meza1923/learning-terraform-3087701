4fb2475ab16fcd51f85f1462fda1318e5f3ceee2
Meza1923 <97029572+Meza1923@users.noreply.github.com>
2024-11-27 14:27:08 +0200

    chagning module version

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
+  version = "4.13.0"
   
 
   vpc_id = module.blog_vpc.vpc_id
