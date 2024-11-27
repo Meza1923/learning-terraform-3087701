0fd4b626ca2d915755b802e8fe315e9c19879392
Meza1923 <97029572+Meza1923@users.noreply.github.com>
2024-11-27 19:37:49 +0200

    fixing typo

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ module "autoscaling" {
   max_size = 1
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_groups_args  = module.blog_alb
+  target_group_arns  = module.blog_alb
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id               = data.aws_ami.app_ami.id
