93504498b8367dc9314d5b202938e562bbdf1cb4
Meza1923 <97029572+Meza1923@users.noreply.github.com>
2024-11-27 19:39:39 +0200

    changing to string

--- main.tf
+++ main.tf
@@ -35,12 +35,12 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
   
-  name     = blog
+  name     = "blog"
   min_size = 1
   max_size = 1
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns  = module.blog_alb
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id               = data.aws_ami.app_ami.id
